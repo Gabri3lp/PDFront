d5d66fb40043b19974196bac8bed641c955faa69
Gabriel <Gabriel.jp215@gmail.com>
2019-01-13 23:03:27 -0400

    Casi terminado

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -20,9 +20,9 @@ export class SearchService {
     
     let params = new HttpParams();
     if(page.IsHighlighted){
-      params = params.append("IsHighlighted", "true")
       params = params.append("Id", "" + page.Id)
     }else{
+      params = params.append("Id", "" + (-1))
       params = params.append("IsHighlighted", "false")
       params = params.append('Title', page.Title);
       params = params.append('Snippet', page.Snippet);
@@ -30,7 +30,7 @@ export class SearchService {
       params = params.append('Query', this.currentSearch);
     }
   
-    this.http.get(BACKEND_API_URL+"/highlight", {params: params}).subscribe(result =>{if(result) page.IsHighlighted = true;});
+    this.http.get(BACKEND_API_URL+"/highlight", {params: params}).subscribe(result =>{if(result) page.IsHighlighted = !page.IsHighlighted;});
   } 
   search(query: string): Observable<Page[]>{
     if(query == "")
